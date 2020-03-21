a95461db7fee53ca0c6ec20d6f67d2e7c5359273
awarnecke <a.warnecke@sec.tu-bs.de>
2020-03-21 17:08:50 +0100

    Forward pass with while loop.

--- calculate_relevances.py
+++ calculate_relevances.py
@@ -3,14 +3,13 @@ import tensorflow as tf
 from lstm_network import LSTM_network
 
 if __name__ == '__main__':
-    T = 5
-    n_hidden = 3
+    T = 20
+    n_hidden = 30
     n_embedding = 10
     n_classes = 2
-    batch_size = 2
+    batch_size = 3
     net = LSTM_network(n_hidden, n_embedding, n_classes, batch_size)
-    print(net.c_forward)
     x = tf.constant(np.random.randn(batch_size,n_embedding))
     input = tf.constant(np.random.randn(batch_size, T, n_embedding))
-    input_rev = tf.reverse(input, [1])
-    f, b = net.one_step(x)
+    f_b = net.one_step(x)
+    output = net.forward_pass(input)
--- lstm_network.py
+++ lstm_network.py
@@ -9,6 +9,7 @@ class LSTM_network:
         self.n_hidden = n_hidden
         self.embedding_dim = embedding_dim
         self.n_classes = n_classes
+        self.batch_size = batch_size
 
         self.W_x_forward = tf.constant(np.random.randn(self.embedding_dim, 4 * self.n_hidden))
         self.W_h_forward = tf.constant(np.random.randn(self.n_hidden, 4 * self.n_hidden))
@@ -18,12 +19,12 @@ class LSTM_network:
         self.W_h_backward = tf.constant(np.random.randn(self.n_hidden, 4 * self.n_hidden))
         self.b_backward = tf.constant(np.random.randn(4 * self.n_hidden, ))
 
-        self.W_hidden = tf.constant(np.random.randn(n_hidden, n_classes))
+        self.W_dense = tf.constant(np.random.randn(n_hidden, n_classes))
 
-        self.h_forward = tf.Variable(np.zeros((batch_size, n_hidden)))
-        self.c_forward = tf.Variable(np.zeros((batch_size, n_hidden)))
-        self.h_backward = tf.Variable(np.zeros((batch_size, n_hidden)))
-        self.c_backward = tf.Variable(np.zeros((batch_size, n_hidden)))
+        self.h_forward = tf.Variable(np.zeros((self.batch_size, n_hidden)))
+        self.c_forward = tf.Variable(np.zeros((self.batch_size, n_hidden)))
+        self.h_backward = tf.Variable(np.zeros((self.batch_size, n_hidden)))
+        self.c_backward = tf.Variable(np.zeros((self.batch_size, n_hidden)))
 
         self.idx_i = slice(0, self.n_hidden)
         self.idx_f = slice(self.n_hidden, 2 * self.n_hidden)
@@ -34,22 +35,32 @@ class LSTM_network:
     @tf.function
     def cell_step(self, x, h, c, W_x, W_h, b):
         # forward pass
-        gate_forward_x = tf.matmul(x, W_x)
-        gate_forward_h = tf.matmul(h, W_h)
-        gate_forward_pre = gate_forward_x + gate_forward_h + b
-        print(gate_forward_pre.shape)
-        gate_forward_post = tf.concat([
-                            tf.sigmoid(gate_forward_pre[:,self.idx_i]), tf.sigmoid(gate_forward_pre[:, self.idx_f]),
-                            tf.tanh(gate_forward_pre[:,self.idx_c]), tf.sigmoid(gate_forward_pre[:, self.idx_o]),
+        gate_x = tf.matmul(x, W_x)
+        gate_h = tf.matmul(h, W_h)
+        gate_pre = gate_x + gate_h + b
+        gate_post = tf.concat([
+                            tf.sigmoid(gate_pre[:,self.idx_i]), tf.sigmoid(gate_pre[:, self.idx_f]),
+                            tf.tanh(gate_pre[:,self.idx_c]), tf.sigmoid(gate_pre[:, self.idx_o]),
                             ], axis=1)
-        print(gate_forward_post.shape)
-        c.assign(gate_forward_post[:, self.idx_f]*c + gate_forward_post[:, self.idx_i]*gate_forward_post[:, self.idx_c])
-        h.assign(gate_forward_post[:, self.idx_o] * tf.tanh(c))
-        return gate_forward_pre, gate_forward_post, c, h
+        c.assign(gate_post[:, self.idx_f] * c + gate_post[:, self.idx_i] * gate_post[:, self.idx_c])
+        h.assign(gate_post[:, self.idx_o] * tf.tanh(c))
+        return gate_pre, gate_post, c, h
 
 
     @tf.function
-    def one_step(self, x):
+    def one_step(self, x):#, x_rev):
         forward = self.cell_step(x, self.h_forward, self.c_forward, self.W_x_forward, self.W_h_forward, self.b_forward)
-        backward = self.cell_step(x, self.h_backward, self.c_backward, self.W_x_backward, self.W_h_backward, self.b_backward)
-        return forward, backward
+        #backward = self.cell_step(x_rev, self.h_backward, self.c_backward, self.W_x_backward, self.W_h_backward, self.b_backward)
+        return forward#, backward
+
+    @tf.function
+    def forward_pass(self, x):
+        cond = lambda i, p: tf.less(i, tf.shape(x)[1])
+        output = (tf.zeros((self.batch_size, 4 * self.n_hidden), dtype=tf.float64),
+                  tf.zeros((self.batch_size, 4 * self.n_hidden), dtype=tf.float64),
+                  tf.zeros((self.batch_size, self.n_hidden), dtype=tf.float64),
+                  tf.zeros((self.batch_size, self.n_hidden), dtype=tf.float64))
+        loop_vars = (tf.constant(0), output)
+        body = lambda i, p: (tf.add(i, 1), self.one_step(x[:,i,:]))
+        o = tf.while_loop(cond, body, loop_vars)
+        return o[1]
