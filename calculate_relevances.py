e0c14d6453afc1ee6f6369a64a25a9d58f391d46
awarnecke <a.warnecke@sec.tu-bs.de>
2020-03-24 06:54:55 +0100

    Now works but function retracing has to be fixed.

--- calculate_relevances.py
+++ calculate_relevances.py
@@ -3,13 +3,15 @@ import tensorflow as tf
 from lstm_network import LSTM_network
 
 if __name__ == '__main__':
-    T = 20
-    n_hidden = 30
-    n_embedding = 10
+    T = 5
+    n_hidden = 3
+    n_embedding = 2
     n_classes = 2
-    batch_size = 3
+    batch_size = 4
+    eps = 1e-3
     net = LSTM_network(n_hidden, n_embedding, n_classes, batch_size)
     x = tf.constant(np.random.randn(batch_size,n_embedding))
+    y = np.zeros((batch_size,))
     input = tf.constant(np.random.randn(batch_size, T, n_embedding))
-    f_b = net.one_step(x)
-    output = net.forward_pass(input)
+    #output = net.full_pass(input)
+    net.lrp(input, y, eps)
