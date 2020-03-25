10b0135b265d9b4be2b09336f6920aa842cfe75e
awarnecke <a.warnecke@sec.tu-bs.de>
2020-03-25 09:34:44 +0100

    Forward pass works correctly. Nasty bug ; tf.scan's last output is in entry 0 if reverse is True...

--- calculate_relevances.py
+++ calculate_relevances.py
@@ -3,23 +3,52 @@ import tensorflow as tf
 import time
 from lstm_network import LSTM_network
 from tqdm import tqdm
+from keras.models import Sequential
+from keras.layers import Bidirectional, LSTM, Dense
 
-if __name__ == '__main__':
-    n_samples = 2500
-    T = 50
-    n_hidden = 300
-    n_embedding = 200
-    n_classes = 2
-    batch_size = 25
-    eps = 1e-3
+
+def get_dummy_model(units, embedding_dim, n_classes):
+    model = Sequential()
+    model.add(Bidirectional(LSTM(units), input_shape=(None, embedding_dim)))
+    model.add(Dense(n_classes))
+    return model
+
+
+def test_equality():
+    orig_model = get_dummy_model(n_hidden, n_embedding, n_classes)
+    weights = orig_model.get_weights()
+    net = LSTM_network(n_hidden, n_embedding, n_classes, batch_size, weights)
+    input_keras = np.random.randn(batch_size, T, n_embedding)
+    input_tf = tf.constant(input_keras)
+    net.full_pass(input_tf)
+    model_output = orig_model.predict(input_keras)
+    net_output = net.y_hat
+    print(model_output)
+    print(net_output.numpy())
+    return np.allclose(net_output.numpy(), model_output, atol=1e-2)
+
+
+def test_time():
     net = LSTM_network(n_hidden, n_embedding, n_classes, batch_size)
     input = tf.constant(np.random.randn(n_samples, T, n_embedding))
     start = time.time()
     for i in tqdm(range(0, n_samples, batch_size)):
         Rx, rest = net.lrp(input[i:i+batch_size], eps=eps)
-    print(Rx.shape)
-    print(rest.shape)
     end = time.time()
     total = end-start
     per_sample = total / n_samples
     print('Processing {} samples took {} s. {} seconds per sample'.format(n_samples, total, per_sample))
+
+
+if __name__ == '__main__':
+    n_samples = 100
+    T = 50
+    n_hidden = 300
+    n_embedding = 200
+    n_classes = 2
+    batch_size = 1
+    eps = 1e-3
+    #path_to_model = '../LRPForSecurity/NetworkTraining/VulDeePecker/models/keras_model_wo_metrics_w_softmax.hdf5'
+    if test_equality():
+        print('Forward pass of model is correct.')
+    test_time()
