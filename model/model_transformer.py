0e9d0c5c5636481c4a7dea5d90452668d8f07dc4
awarnecke <a.warnecke@sec.tu-bs.de>
2020-04-06 07:42:56 +0200

    Model transformation scripts and parameters

---
+++ model/model_transformer.py
@@ -0,0 +1,24 @@
+import pickle as pkl
+import numpy as np
+import argparse
+
+
+def permute(weights, order):
+    no_units = int(weights.shape[-1]/4)
+    weights_reshaped = weights.reshape((-1, 4, no_units))
+    new_weights_stacked = [weights_reshaped[:,i] for i in order]
+    new_weights = np.stack(new_weights_stacked, axis=1)
+    new_weights = new_weights.reshape((-1,4*no_units))
+    return np.squeeze(new_weights)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('weight_path', type=str, help='Path to .pkl file storing list of weights.')
+    parser.add_argument('order', type=int, nargs=4, help='Permutation order, e.g. [0,2,1,3] swaps second'
+                                                        'and third quarter of the weights.')
+    args = parser.parse_args()
+    weights_old = pkl.load(open(args.weight_path, 'rb'))
+    # last entry is final weight matrix which does not have to be permuted
+    weights_new = [permute(w, args.order) for w in weights_old[:-1]]
+    weights_new.append(weights_old[-1])
+    pkl.dump(weights_new, open('weights_permuted.pkl', 'wb'))
